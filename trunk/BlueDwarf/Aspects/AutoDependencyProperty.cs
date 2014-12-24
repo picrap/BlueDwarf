cd0f102e79fe69e1f0eb355fa810fd9243e5ceef
picrap@gmail.com <picrap@gmail.com@b9c079fe-e3d0-2746-e661-423e995d4945>
2014-12-24 13:35:24 +0000

    Added comments (you're welcome)

--- trunk/BlueDwarf/Aspects/AutoDependencyProperty.cs
+++ trunk/BlueDwarf/Aspects/AutoDependencyProperty.cs
@@ -7,6 +7,10 @@ using PostSharp.Reflection;
 
 namespace BlueDwarf.Aspects
 {
+    /// <summary>
+    /// Marks a simple auto property to be bound to a DependencyProperty
+    /// This attribute is handled by Postsharp, and by some unexplainable magic, works.
+    /// </summary>
     [Serializable]
     [MulticastAttributeUsage(MulticastTargets.Property, AllowMultiple = false, TargetMemberAttributes = MulticastAttributes.NonAbstract | MulticastAttributes.NonLiteral, PersistMetaData = true)]
     public class AutoDependencyProperty : LocationInterceptionAspect
@@ -28,24 +32,40 @@ namespace BlueDwarf.Aspects
         /// </value>
         public AutoDependencyPropertyNotification Notification { get; set; }
 
+        /// <summary>
+        /// Initializes the current aspect.
+        /// </summary>
+        /// <param name="locationInfo">Location to which the current aspect is applied.</param>
         public override void RuntimeInitialize(LocationInfo locationInfo)
         {
             locationInfo.PropertyInfo.CreateDependencyProperty(DefaultValue, Notification);
         }
 
+        /// <summary>
+        /// Method invoked <i>instead</i> of the <c>Get</c> semantic of the field or property to which the current aspect is applied,
+        /// i.e. when the value of this field or property is retrieved.
+        /// </summary>
+        /// <param name="args">Advice arguments.</param>
         public override void OnGetValue(LocationInterceptionArgs args)
         {
             var dependencyProperty = args.Location.PropertyInfo.GetDependencyProperty();
             var dependencyObject = (DependencyObject)args.Instance;
+            // yes, in the end, it is a GetValue()
             args.Value = dependencyObject.GetValue(dependencyProperty);
         }
 
+        /// <summary>
+        /// Method invoked <i>instead</i> of the <c>Set</c> semantic of the field or property to which the current aspect is applied,
+        /// i.e. when the value of this field or property is changed.
+        /// </summary>
+        /// <param name="args">Advice arguments.</param>
         public override void OnSetValue(LocationInterceptionArgs args)
         {
             var dependencyProperty = args.Location.PropertyInfo.GetDependencyProperty();
             var dependencyObject = (DependencyObject)args.Instance;
             var oldValue = dependencyObject.GetValue(dependencyProperty);
             var newValue = args.Value;
+            // not sure it is necessary to check for a change
             if (!oldValue.SafeEquals(newValue))
                 dependencyObject.SetValue(dependencyProperty, newValue);
         }
--- trunk/BlueDwarf/Aspects/AutoNotifyPropertyChangedExtensions.cs
+++ trunk/BlueDwarf/Aspects/AutoNotifyPropertyChangedExtensions.cs
@@ -3,14 +3,30 @@ using BlueDwarf.Utility;
 
 namespace BlueDwarf.Aspects
 {
+    /// <summary>
+    /// Extensions to make it more handy (because we're pragmatic people)
+    /// </summary>
     public static class AutoNotifyPropertyChangedExtensions
     {
+        /// <summary>
+        /// Gets the property category, as an object.
+        /// </summary>
+        /// <param name="viewModel">The view model.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns></returns>
         public static object GetPropertyCategory(this ViewModel.ViewModel viewModel, string propertyName)
         {
             var notifyPropertyChangedAttribute = viewModel.GetType().GetProperty(propertyName).GetCustomAttribute<AutoNotifyPropertyChanged>();
             return notifyPropertyChangedAttribute.Category;
         }
 
+        /// <summary>
+        /// Gets the property category, as the requested type (whose only pupose is to avoid casts in client calls).
+        /// </summary>
+        /// <typeparam name="TCategory">The type of the category.</typeparam>
+        /// <param name="viewModel">The view model.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns></returns>
         public static TCategory GetPropertyCategory<TCategory>(this ViewModel.ViewModel viewModel, string propertyName)
         {
             try
