f04067f6fb060e588fbbfe68a941ea655e428a93
Pascal Craponne <pascal@craponne.fr>
2015-01-23 08:12:23 +0000

    Fixed HostScanner

---
+++ BlueDwarf.Core/Net/Proxy/Scanner/IHostScanner.cs
@@ -0,0 +1,18 @@
+﻿// This is the blue dwarf
+// more information at https://code.google.com/p/blue-dwarf/
+
+namespace BlueDwarf.Net.Proxy.Scanner
+{
+    using System.Collections.Generic;
+
+    public interface IHostScanner
+    {
+        /// <summary>
+        /// Scans the specified page text.
+        /// </summary>
+        /// <param name="pageText">The page text.</param>
+        /// <param name="hostPortEx">The host port regular expression, or null to use internal capture.</param>
+        /// <returns></returns>
+        IEnumerable<HostPort> Scan(string pageText, string hostPortEx = null);
+    }
+}
--- BlueDwarf.Test/HostScannerTest.cs
+++ BlueDwarf.Test/HostScannerTest.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BlueDwarf.Test
 {
+    using System.Linq;
+    using System.Net;
     using System.Text.RegularExpressions;
     using Net.Proxy.Scanner;
 
@@ -12,7 +14,7 @@ namespace BlueDwarf.Test
         [TestMethod]
         public void ParsePortsTest()
         {
-            var portRegex = new Regex(HostScanner.PortRangeEx);
+            var portRegex = new Regex(@"^" + HostScanner.PortRangeEx + HostScanner.AfterDigitEx);
             //var portRegex = new Regex(@"(?<port>(\d{1,4}))");
             for (int i = 0; i <= 65535; i++)
             {
@@ -27,6 +29,42 @@ namespace BlueDwarf.Test
             Assert.IsFalse(portRegex.Match("65600").Success);
             Assert.IsFalse(portRegex.Match("66000").Success);
             Assert.IsFalse(portRegex.Match("70000").Success);
+        }
+
+        [TestMethod]
+        public void ParseIP_0_0_0_0_Test()
+        {
+            var ipV4Regex = new Regex(@"^" + HostScanner.IPv4Ex + HostScanner.AfterDigitEx);
+            var match = ipV4Regex.Match("0.0.0.0");
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void ParseIP_255_255_255_255_Test()
+        {
+            var ipV4Regex = new Regex(@"^" + HostScanner.IPv4Ex + HostScanner.AfterDigitEx);
+            var match = ipV4Regex.Match("255.255.255.255");
+            Assert.IsTrue(match.Success);
+        }
+
+        [TestMethod]
+        public void ParseHostPort_123_45_67_89_7654_Test()
+        {
+            var ipV4Regex = new Regex(HostScanner.IPColonPortEx);
+            var match = ipV4Regex.Match("123.45.67.89:7654");
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual("123.45.67.89", match.Groups["address"].Value);
+            Assert.AreEqual("7654", match.Groups["port"].Value);
+        }
+
+        [TestMethod]
+        public void ParsePageTest()
+        {
+            var hostScanner = new HostScanner();
+            var hosts = hostScanner.Scan("here 123.45.67.89:3128 there 13.24.35.46 : 57 and everywhere 98.76.54.32  1").ToArray();
+            Assert.IsTrue(hosts.Contains(new HostPort(IPAddress.Parse("123.45.67.89"), 3128)));
+            Assert.IsTrue(hosts.Contains(new HostPort(IPAddress.Parse("13.24.35.46"), 57)));
+            Assert.IsTrue(hosts.Contains(new HostPort(IPAddress.Parse("98.76.54.32"), 1)));
         }
     }
 }
