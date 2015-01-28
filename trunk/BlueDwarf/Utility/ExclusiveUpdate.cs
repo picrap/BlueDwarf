421731495bde305df9285f0acf4371f6d9fedb7e
picrap@gmail.com <picrap@gmail.com@b9c079fe-e3d0-2746-e661-423e995d4945>
2015-01-28 12:18:03 +0000

    Added an ExclusiveUpdate in order to avoid infinite loops when reading and writing from controls

---
+++ trunk/BlueDwarf/Utility/ExclusiveUpdate.cs
@@ -0,0 +1,39 @@
+﻿
+namespace BlueDwarf.Utility
+{
+    using System;
+    using System.Reflection;
+    using PostSharp.Aspects;
+    using PostSharp.Aspects.Configuration;
+    using PostSharp.Extensibility;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    [MulticastAttributeUsage(MulticastTargets.Method, PersistMetaData = true)]
+    [Serializable]
+    [MethodInterceptionAspectConfiguration]
+    public class ExclusiveUpdate : Aspect, IMethodInterceptionAspect
+    {
+        public void RuntimeInitialize(MethodBase method)
+        {
+            var methodInfo = method as MethodInfo;
+            if (methodInfo != null && methodInfo.ReturnType != (typeof(void)))
+                throw new InvalidOperationException("ExclusiveUpdate can only be applied to void methods");
+        }
+
+        public void OnInvoke(MethodInterceptionArgs args)
+        {
+            var locked = InstanceData.GetData<bool>(args.Instance, "locked");
+            if (locked)
+                return;
+            try
+            {
+                InstanceData.SetData(args.Instance, "locked", true);
+                args.Proceed();
+            }
+            finally
+            {
+                InstanceData.SetData(args.Instance, "locked", false);
+            }
+        }
+    }
+}
